6fc588ae5aa841302e1b19554aa38df8673f39ee
owlbytes <neshah07@gmail.com>
2014-06-20 09:17:25 +0100

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tda::Application.config.secret_token = 'a8a2b9f86e72ebda917fd14e1f1fe144bc2a6a5661cf374906a6c89954e3ebdcdf7963ff54224016e2c2b5f0302b38f69c66bc7f573b7b11850b88b79a2cb751'
